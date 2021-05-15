a9f4c5213b47fdb469207b85485daa42d8fb68c5
ait <ait@ex.in>
2021-05-15 13:45:50 +0900

    Set infinity Scroll

--- lib/main.dart
+++ lib/main.dart
@@ -1,44 +1,38 @@
+// Copyright 2018 The Flutter team. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 import 'package:flutter/material.dart';
-import 'package:english_words/english_words.dart'; // Add this line.
+import 'package:english_words/english_words.dart';
 
 void main() => runApp(MyApp());
 
+// #docregion MyApp
 class MyApp extends StatelessWidget {
+  // #docregion build
   @override
   Widget build(BuildContext context) {
-    // final wordPair = WordPair.random(); // Add this line.
     return MaterialApp(
-      title: 'Welcome to Flutter',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Welcome to Flutter'),
-        ),
-        body: Center(
-          // child: const Text('Hello World'),
-          // child: Text(wordPair.asPascalCase),  // With this text.
-          child: RandomWords(), // ...this line
-        ),
-      ),
+      title: 'Startup Name Generator',
+      home: RandomWords(),
     );
   }
+// #enddocregion build
 }
+// #enddocregion MyApp
 
-class RandomWords extends StatefulWidget {
-  @override
-  _RandomWordsState createState() => _RandomWordsState();
-}
-
+// #docregion RWS-var
 class _RandomWordsState extends State<RandomWords> {
-  final _suggestions = <WordPair>[]; // NEW
-  final _biggerFont = const TextStyle(fontSize: 18); // NEW
+  final _suggestions = <WordPair>[];
+  final _biggerFont = TextStyle(fontSize: 18.0);
+  // #enddocregion RWS-var
 
   // #docregion _buildSuggestions
   Widget _buildSuggestions() {
     return ListView.builder(
         padding: EdgeInsets.all(16.0),
         itemBuilder: /*1*/ (context, i) {
-          if (i.isOdd) return Divider();
-          /*2*/
+          if (i.isOdd) return Divider(); /*2*/
 
           final index = i ~/ 2; /*3*/
           if (index >= _suggestions.length) {
@@ -47,7 +41,6 @@ class _RandomWordsState extends State<RandomWords> {
           return _buildRow(_suggestions[index]);
         });
   }
-
   // #enddocregion _buildSuggestions
 
   // #docregion _buildRow
@@ -59,9 +52,9 @@ class _RandomWordsState extends State<RandomWords> {
       ),
     );
   }
-
   // #enddocregion _buildRow
 
+  // #docregion RWS-build
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -70,8 +63,13 @@ class _RandomWordsState extends State<RandomWords> {
       ),
       body: _buildSuggestions(),
     );
-    //return Container();
-    // final wordPair = WordPair.random(); // NEW
-    // return Text(wordPair.asPascalCase); // NEW
   }
+// #enddocregion RWS-build
+// #docregion RWS-var
+}
+// #enddocregion RWS-var
+
+class RandomWords extends StatefulWidget {
+  @override
+  State<RandomWords> createState() => _RandomWordsState();
 }
